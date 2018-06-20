5504624b39c3c5da746ce62feef8f3fc0fe2a137
Bangaly KAKE <bangaly.kake@sis-marches.com>
2018-06-20 16:34:51 +0200

    Création de RootQuery

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,5 +1,12 @@
 const graphQL = require("graphql");
-const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphQL;
+const lodash = require("lodash");
+const {GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLInt } = graphQL;
+
+const users = [
+    {id: '1', firstName: 'Kake', age: 25},
+    {id: '2', firstName: 'Amc', age: 55},
+    {id: '3', firstName: 'Lolo', age: 35}
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -8,4 +15,21 @@ const UserType = new GraphQLObjectType({
         firstName : { type : GraphQLString},
         age: { type: GraphQLInt}
     }
-})
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields : {
+        user: {
+            type: UserType,
+            args: {id : {type: GraphQLString}},
+            resolve(parentValue, args) {
+               return lodash.find(users, {id: args.id})
+            }
+        }
+    }  
+});
+
+module.exports = new GraphQLSchema({
+    query : RootQuery
+});
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const expressGraphQL = require('express-graphql');
+const userSchema = require('./schemas/schema');
+
 const server = express();
 
 server.use("/salutGraphQL", expressGraphQL({
-    graphiql:true
+    graphiql:true,
+    schema: userSchema
 }));
 server.listen(4000, () => {
     console.log('Serveur est en écoute sur le port 4000');
